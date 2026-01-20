568a78b2263b1c0a7559e0a22d10c6fed0131964
kawax <kawaxbiz@gmail.com>
2026-01-21 08:48:23 +0900

    Update console.php

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Process;
+use Revolution\Copilot\CopilotSession;
+use Revolution\Copilot\Facades\Copilot;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
@@ -11,5 +14,10 @@ use Illuminate\Support\Facades\Artisan;
 
 // vendor/bin/testbench copilot:ping
 Artisan::command('copilot:ping', function () {
-
+    Copilot::start(function (CopilotSession $session) {
+        dump($session->id());
+    });
+    //    $client = Copilot::getClient();
+    //    $client->start();
+    //    dump($client->ping());
 });
