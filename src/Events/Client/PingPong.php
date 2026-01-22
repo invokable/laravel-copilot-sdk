176f5c1023c96cf2e340d3f8a3615ab42f8f5621
kawax <kawaxbiz@gmail.com>
2026-01-22 15:11:17 +0900

    pint

--- src/Events/Client/PingPong.php
+++ src/Events/Client/PingPong.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Revolution\Copilot\Events\Client;
 
 use Illuminate\Foundation\Events\Dispatchable;
-use Revolution\Copilot\Client;
 
 class PingPong
 {
--- src/Events/Session/MessageSendAndWait.php
+++ src/Events/Session/MessageSendAndWait.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Revolution\Copilot\Events\Session;
 
 use Illuminate\Foundation\Events\Dispatchable;
-use Revolution\Copilot\JsonRpc\JsonRpcMessage;
 use Revolution\Copilot\Types\SessionEvent;
 
 class MessageSendAndWait
