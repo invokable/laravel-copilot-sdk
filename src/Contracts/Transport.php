e4357c04aa0141832a4c276f7fb1438f7d7979cc
kawax <kawaxbiz@gmail.com>
2026-01-25 07:28:31 +0900

    Update Transport.php

--- src/Contracts/Transport.php
+++ src/Contracts/Transport.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Revolution\Copilot\Contracts;
 
+use Closure;
+
 interface Transport
 {
     public function send(string $message): void;
 
     public function read(float $timeout = 0.1): string;
+
+    public function stream(Closure $stream): void;
 }
